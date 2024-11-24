fb5a400aa15593e4f4bb4c0b87882f17fdd89f06
Erdi Arikan <88897583+erdiarikan@users.noreply.github.com>
2024-11-24 06:35:57 +0000

    mailgun installed

--- config/mail.php
+++ config/mail.php
@@ -95,6 +95,12 @@ return [
             ],
         ],
 
+        'mailgun' => [
+            'domain' => env('MAILGUN_DOMAIN'),
+            'secret' => env('MAILGUN_SECRET'),
+            'endpoint' => env('MAILGUN_ENDPOINT', 'api.eu.mailgun.net'),
+        ],
+
     ],
 
     /*
